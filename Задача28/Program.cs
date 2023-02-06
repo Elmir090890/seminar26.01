2b9d53dc094cf0cfd6ff8744bbc7de79c8fcce5f
«Elmir» <elmir090890@mail.ru>
2023-02-06 21:20:36 +0300

    Задача 28

---
+++ Задача28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Решение в группах задач:
+// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.Clear();
+
+int Multi(int number)
+{
+    int prod = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        prod *= i;
+    }
+    return prod;
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"Произведения чисел от 1 до {N} равно {Multi(N)}");
