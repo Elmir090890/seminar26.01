3b4f501495287ec120bdd290f61df38231201526
«Elmir» <elmir090890@mail.ru>
2023-02-09 12:30:49 +0300

    add 2

---
+++ Задача26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.Clear();
+
+int NumLen(int A)
+{
+    int count = 0;
+    while (A > 0)
+    {
+        A = A / 10;
+        count = count + 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Enter your Number: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Tne number of digits of your number {N} is {NumLen(N)}");
