5f38432b0cf51086780e1262d5ca610b9f5b37bf
«Elmir» <elmir090890@mail.ru>
2023-02-20 19:56:47 +0300

    Задача 43

---
+++ Задача43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2
+// и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// у = k1 * x + b1, y = k2 * x + b2
+// k1 * x + b1 = k2 * x +b2
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 -k2)
+// y = k1 * x + b1
+
+Console.Clear();
+
+double ResaltX(int b1, int k1, int b2, int k2)
+{
+    // double x = (b2 -b1) / (k1 - k2)
+    double x1 = b2 -b1;
+    double x2 = k1 - k2;
+    double x = x1 / x2;
+    return x;
+}
+
+double ResaltY(int k1, int b1, double x)
+{
+    double y = (k1 * x) + b1;
+    return y;
+}
+Console.Write("Введите значения b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значения k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значения b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значения k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+
+double x = ResaltX(b1, k1, b2, k2);
+double y = ResaltY(k1, b1, x);
+
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} ->({x}; {y})");
+
+// b1 = 2, k1 =5, b2 = 4 ,k2 =9 -> (-0,5 ; -0,5)
